275d53c96c460146cdc423b689feb3945c8b03fe
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2019-05-12 17:15:55 +0200

    V1
    First version of my gulp configuration

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # MyGulpConfig
+
+## Introduction
+It's my gulp 4 configuration when i'm developping.
+For now it's the first version so it can have some problems.
+Feel free to suggest improvements or fork it to customize it!
+
+## Installation
+
+1. Install Node.js
+First, you need to install node.js on your system.
+Visit https://nodejs.org/
+
+2. Clone the repository
+
+3. Install dependencies of npm using
+```bash
+npm i
+```
+
+4. Configure browserSync
+Edit the file `gulpfile.js` at the root of the projet at lines 18 and 19 to setup the link and the port to use by browserSync like below.
+
+```javascript
+const browserLink = 'http://localhost/'
+const browserPort = 80
+```
+
+5. You're ready to go !
+
+
+
+## Commandes
+
+##### Browser sync
+When developing, use browser sync to speed up your productivity.
+It'll compile the scss, prefixes it, optimize images and copy scripts/html/PHP in the public folder.
+```bash
+gulp sync
+```
+You can use these aliases:
+- `gulp devSync`
+- `gulp devsync`
+- `gulp dev`
+
+##### Build files in dev mode
+
+To manualy execute the build task use in `gulp sync`, use one of these commands :
+- `gulp devBuild`
+- `gulp devbuild`.
+
+##### Build before push to production
+Optimize your project before upload it !
+It'll :
+- compile sass
+- add prefixes to your css
+- concat the css and scripts
+- minify css
+- fully optimize images (with little lossy compression)
+
+Use one of these aliases to do this :
+- `gulp prodBuild`
+- `gulp prodbuild`.
+
+##### Clean the project
+For any reason you what to clean-up the public folder, you can soft clean with `gulp clean`. This won't delete images.
+
+To fully clean the folder and delete all files, use `gulp fullClean` or one of these alisases:
+- `gulp cleanFull`
+- `gulp cleanfull`
+- `gulp fullclean`
---
+++ gulpfile.js
@@ -0,0 +1,275 @@
+const gulp = require('gulp');
+const gulp_sass = require('gulp-sass')
+const gulp_csscomb = require('gulp-csscomb')
+const gulp_prefixer = require('gulp-autoprefixer')
+const gulp_cssnano = require('gulp-cssnano')
+const gulp_compressor = require('gulp-compress')
+const gulp_if = require('gulp-if')
+const gulp_cache = require('gulp-cache')
+const gulp_imagemin = require('gulp-imagemin')
+const imageminJpegoptim = require('imagemin-jpegoptim');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminSvgo = require('imagemin-svgo');
+const browserSync = require('browser-sync').create()
+const del = require("del");
+// const gulp_sourceMaps = require('gulp-sourcemaps')
+
+const browserLink = 'https://localhost/1-SITES/MyGulpConfig/public/'
+const browserPort = 443
+const srcFolder = './src'
+const publicFolder = './public'
+
+const csscombDevConfig = './csscomb_dev_config.json'
+const csscombProdConfig = './csscomb_prod_config.json'
+
+const srcSass = srcFolder + '/styles/**/*.+(scss|sass)'
+const srcJS = srcFolder + '/scripts/*.js'
+const srcHTML = srcFolder + '/pages/html/*.html'
+const srcPHP = srcFolder + '/pages/php/*.php'
+const srcImg = srcFolder + '/imgs/*.*'
+
+const publicStyleDest = publicFolder + '/styles/'
+const publicScriptDest = publicFolder + '/scripts/'
+const publicPagesDest = publicFolder + '/pages/'
+const publicImgsDest = publicFolder + '/imgs/'
+
+// TODO: Create an init function
+// TODO: integrate uncss to styleProd()
+
+function clean(done)
+{
+    del([publicFolder + '/**/*.*', '!' + publicFolder + '/imgs', '!' + publicFolder + '/imgs/**/*.*', '!' + publicFolder + '/imgs/**/*'])
+    done();
+}
+
+function cleanFull(done)
+{
+    del(publicFolder + '/**/*.*')
+    done();
+}
+
+const isIndex = function(file)
+{
+    if (file.basename == 'index.html' || file.basename == 'index.php') { return true }
+    else { return false }
+}
+
+const isPage = function(file)
+{
+    if (file.basename != 'index.html' && file.basename != 'index.php') { return true }
+    else { return false }
+}
+
+
+/********************
+*   Dev functions   *
+********************/
+
+function styleDev()
+{
+    return gulp.src(srcSass)
+    // // .pipe(gulp_plumber({ errorHandler: onError }))
+    .pipe(gulp_sass())
+    .pipe(gulp_prefixer())
+    .pipe(gulp_csscomb(csscombDevConfig))       // PB config not used https://github.com/koistya/gulp-csscomb/issues/37 // TODO: resolv, csscomb config pb
+    // // .pipe(gulp_notify("Sass compiled !"))
+    .pipe(gulp.dest(publicStyleDest))
+}
+
+// TODO: FIX IT !
+function beautifySCSS()
+{
+    return gulp.src(srcSass)
+    .pipe(gulp_csscomb(csscombDevConfig))       // PB config not used https://github.com/koistya/gulp-csscomb/issues/37 // TODO: resolv, csscomb config pb
+    .pipe(gulp.dest(srcSass))
+}
+
+function scriptDev()
+{
+    return gulp.src(srcJS)
+    .pipe(gulp.dest(publicScriptDest))
+}
+
+function htmlDev()
+{
+    return gulp.src(srcHTML)
+    .pipe(gulp_if(isIndex, gulp.dest(publicFolder)))
+    .pipe(gulp_if(isPage, gulp.dest(publicPagesDest)))
+}
+
+function phpDev()
+{
+    return gulp.src(srcPHP)
+    .pipe(gulp_if(isIndex, gulp.dest(publicFolder)))
+    .pipe(gulp_if(isPage, gulp.dest(publicPagesDest)))
+}
+
+const pagesDev = gulp.parallel(htmlDev, phpDev)
+
+function imgDev()
+{
+    return gulp.src(srcImg)
+    .pipe(gulp_cache(gulp_imagemin([
+                                    gulp_imagemin.gifsicle({interlaced: true, optimizationLevel: 1, colors: 128}),
+                                    gulp_imagemin.jpegtran({progressive: true}),
+                                    imageminJpegoptim ({progressive: true, strilAll: true}),
+                                    imageminMozjpeg({quality: 50, arithmetic: true, arithmetic: true, smooth: 50}),
+                                    imageminPngquant({speed: 1, strip: true, quality: [0.3, 0.5]}),
+                                    imageminSvgo(),
+                                    ]))
+        )
+    .pipe(gulp.dest(publicImgsDest))
+}
+
+const devBuild = gulp.parallel(styleDev, scriptDev, pagesDev, imgDev)
+
+/*********************
+*   Prod functions   *
+*********************/
+
+function styleProd()
+{
+    return gulp.src(srcSass)
+    .pipe(gulp_sass())
+    .pipe(gulp_prefixer())
+    .pipe(gulp_csscomb())       // PB config not used https://github.com/koistya/gulp-csscomb/issues/37 // TODO: resolv, csscomb config pb
+    .pipe(gulp_cssnano())
+    // .pipe(gulp_compressor.minifyCss())
+    .pipe(gulp.dest(publicStyleDest))
+}
+
+function scriptProd()
+{
+    return gulp.src(srcJS)
+    .pipe(gulp_compressor.minifyJs())
+    .pipe(gulp.dest(publicScriptDest))
+}
+
+function htmlProd()
+{
+    return gulp.src(srcHTML)
+    .pipe(gulp_if(isIndex, gulp.dest(publicFolder)))
+    .pipe(gulp_if(isPage, gulp.dest(publicPagesDest)))
+}
+
+function phpProd()
+{
+    return gulp.src(srcPHP)
+    .pipe(gulp_if(isIndex, gulp.dest(publicFolder)))
+    .pipe(gulp_if(isPage, gulp.dest(publicPagesDest)))
+}
+
+const pagesProd = gulp.parallel(htmlProd, phpProd)
+
+function imgProd()
+{
+    return gulp.src(srcImg)
+    .pipe(gulp_cache(gulp_imagemin([
+                                    gulp_imagemin.gifsicle({interlaced: true, optimizationLevel: 3, colors: 128}),
+                                    gulp_imagemin.jpegtran({progressive: true, arithmetic: true}),
+                                    gulp_imagemin.optipng({optimizationLevel: 7}),
+                                    imageminJpegoptim ({progressive: true, strilAll: true}),
+                                    imageminMozjpeg({quality: 50, arithmetic: true, arithmetic: true, smooth: 50}),
+                                    imageminPngquant({speed: 1, strip: true, quality: [0.3, 0.5]}),
+                                    imageminSvgo({
+                                                    plugins: [
+                                                        {cleanupAttrs: true },
+                                                        {cleanupEnableBackground: true },
+                                                        {cleanupIDs: true },
+                                                        {cleanupNumericValues: true },
+                                                        {cleanupListOfValues: true },
+
+                                                        {collapseGroups: true },
+
+                                                        {convertStyleToAttrs: true },
+                                                        {convertColors: true },
+                                                        {convertPathData: true },
+                                                        {convertTransform: true },
+                                                        {convertShapeToPath: true },
+
+                                                        {inlineStyles: true },
+
+                                                        {mergePaths: true },
+
+                                                        {minifyStyles: true },
+
+                                                        {reusePaths: true },
+
+                                                        {removeDoctype: true },
+                                                        {removeXMLProcInst: true },
+                                                        {removeComments: true },
+                                                        {removeMetadata: true },
+                                                        {removeTitle: true },
+                                                        {removeDesc: true },
+                                                        {removeUselessDefs: true },
+                                                        {removeXMLNS: true },
+                                                        {removeEditorsNSData: true },
+                                                        {removeEmptyAttrs: true },
+                                                        {removeHiddenElems: true },
+                                                        {removeEmptyText: true },
+                                                        {removeEmptyContainers: true },
+                                                        {removeViewBox: true },
+                                                        {removeUnknownsAndDefaults: true },
+                                                        {removeNonInheritableGroupAttrs: true },
+                                                        {removeUselessStrokeAndFill: true },
+                                                        {removeUnusedNS: true },
+                                                        {removeRasterImages: true },
+                                                        {removeDimensions: true },
+                                                        {removeAttrs: true },
+                                                        {removeOffCanvasPaths: true },
+
+                                                        {sortAttrs: true },
+                                                    ]
+                                                }),
+                                  ])
+                    )
+        )
+    .pipe(gulp.dest(publicImgsDest))
+}
+
+const prodBuild = gulp.parallel(styleProd, scriptProd, pagesProd, imgProd)
+
+
+/********************
+*   Browser Sync    *
+********************/
+function browserSyncReload(done)
+{
+  browserSync.reload();
+  done();
+}
+
+function devSync()
+{
+    browserSync.init({
+        proxy: browserLink,
+        port: browserPort,
+    });
+
+    gulp.watch(srcSass, gulp.series(clean, styleDev, browserSyncReload))
+    gulp.watch(srcJS, gulp.series(clean, scriptDev, browserSyncReload))
+    gulp.watch(srcHTML, gulp.series(clean, htmlDev, browserSyncReload))
+    gulp.watch(srcPHP, gulp.series(clean, phpDev, browserSyncReload))
+    gulp.watch(srcImg, gulp.series(clean, imgDev, browserSyncReload))
+    gulp.watch("./**/**/*", browserSyncReload)
+}
+
+
+// exports.default = defaultTask
+exports.prodBuild = gulp.series(cleanFull, devBuild)
+exports.prodbuild = exports.prodBuild
+
+exports.devBuild = gulp.series(clean, prodBuild) // TODO: add beautifySCSS
+exports.devbuild = exports.devBuild
+
+exports.sync = gulp.series(clean, devBuild, devSync)
+exports.devSync = exports.sync
+exports.devsync = exports.sync
+exports.dev = exports.sync
+
+exports.clean = clean
+exports.cleanFull = cleanFull
+exports.cleanfull = cleanFull
+exports.fullClean = cleanFull
+exports.fullclean = cleanFull
